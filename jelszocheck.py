b21fdc94e4fc72f966fa4f245f6839ba12094371
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2025-09-17 10:18:00 +0200

    hőtanács

--- jelszocheck.py
+++ jelszocheck.py
@@ -1,3 +1,6 @@
+"""Kérj be egy jelszót, és ha megegyezik a „titok” szóval, írd ki: „Belépés engedélyezve!”, különben „Helytelen jelszó!”.
+"""
+
 password = input("Kérem a jelszót: ")
 if password == "juhuuu":
     print('A jelszó helyes')
