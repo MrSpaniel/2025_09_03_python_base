a003826a78bd867b46e56db07738d5dd84a89c05
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2025-09-24 08:02:19 +0200

    Update labda.py

--- dolgozat/labda.py
+++ dolgozat/labda.py
@@ -5,10 +5,13 @@ A labda körül a szalag egy kör (kerület képlete 2*r*PI)
 A program kérje be a labda átmérőjét, a labdák számát, és a rendelkezésre álló szalag hosszát!
 Számítsa ki, és írja a képernyőre, hogy a bekért számú labda csomagolásához hány centiméter szalagra van szükség, valamint állapítsa meg,
 hogy elegendő szalagunk van-e a művelet elvégzéséhez, és ezt is közölje a felhasználóval!"""
-
+labdaszam = float(input("Adja meg a labdák számát:"))
 szalaghossz = float(input("Adja meg az önnél lévő szalag hosszát cm-ben:"))
 atmero = float(input("Adja meg a labda átmérőjét cm-ben:"))
 PI = 3.14
-Kerulet = 2*(2* atmero * PI) + 60
-if Kerulet <= szalaghossz print("Az önnél lévő szalag elegendő a labdához!")
-else print("Az önnél lévő szalag nem elegendő!")
+Kerulet = labdaszam * (2*atmero * PI) + 60
+print(f"A labdájához minimum {Kerulet} cm szalagra van szükség!")
+if Kerulet <= szalaghossz:
+   print(f"Az önnél lévő szalag elegendő a(z) {labdaszam} labdához!")
+else :
+   print(f"Az önnél lévő szalag nem elegendő a(z) {labdaszam} labdához!")
